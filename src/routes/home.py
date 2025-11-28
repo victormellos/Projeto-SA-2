b0c93b4e88135e039f0b77d6e83f383aa2d212cb
VICTOR AUGUSTO MELLOS <victor_mellos@estudante.sesisenai.org.br>
2025-11-27 22:05:16 -0300

    Adicionei coisas

--- src/routes/home.py
+++ src/routes/home.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+
+import sqlite3
+
+conn = sqlite3.connect('automax.db')
+cursor = conn.cursor()
 
 @app.route('/home')
 def home():
@@ -13,9 +18,51 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
+    if request.method == 'POST':
+
+        nome = request.form.get("nome")
+        cpf = request.form.get("cpf")
+        celular = request.form.get("celular")
+        email = request.form.get("email")
+        senha = request.form.get("senha")
+
+        
+
+        tipo_usuario = request.form.get("tipo_usuario") 
+
+        match tipo_usuario:
+            case "cliente":
+                placa = request.form.get("placa")
+                modelo = request.form.get("modelo")
+                marca = request.form.get("marca")
+                ano = request.form.get("ano")
+                cor = request.form.get("cor")
+
+                cursor.execute("""
+                INSERT INTO clientes (nome_cliente, CPF, celular, email, senha)
+                VALUES (?, ?, ?, ?, ?)
+                """, (nome, cpf, celular, email, senha))
+
+                cursor.execute("""
+                INSERT INTO veiculos (marca, cor, ano, modelo, placa)
+                VALUES (?, ?, ?, ?, ?)
+                """, (marca, cor, ano, modelo, placa))
+                
+                conn.commit()
+                conn.close()
+
+            case "funcionario":
+                cargo = request.form.get("cargo")
+
+                cursor.execute("""
+                INSERT INTO funcionarios (nome_funcionario, nivel_de_acesso)
+                VALUES (?, ?)
+                """, (nome, cargo))
+                
     return render_template('cadastro.html')
+
 
 # ← só essa linha abaixo
 import routes.produto
